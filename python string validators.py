f431b14e973619f63745c7cd9adc1bd84f6d93a4
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-03-25 17:33:32 +0530

    Add files via upload

---
+++ python string validators.py
@@ -0,0 +1,21 @@
+
+#this method checks if all the charecter of a string are alphanumeric(a-z.A-Z and 0-9)
+#syntax is (variable.isalnum())
+A="sqt2"
+print(A.isalnum())
+#this method checks if all charecter of a string alphabetical(a-z,A-Z)
+#syntax is (variable.isalpha())
+b="Abc"
+print(b.isalpha())
+#this method checks if all charecter of string alphadigits(0-9)
+#syntax is (variable.isdigit())
+c="1234"
+print(c.isdigit())
+#this method check this methode consist only lowercase charecter
+#syntax is (variable.islower())
+d="abc12#"
+print(d.islower())
+#this method check this methode consist only uppercase charecter
+#syntax is (variable.isupper())
+e="ACD234$"
+print(e.isupper())
