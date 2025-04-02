c95d82f262bd205e81cbfc4619553aec63d6b020
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-02 17:51:44 +0530

    Add files via upload

---
+++ maltiline str  and  type casting.py
@@ -0,0 +1,26 @@
+# string casting
+# it is use to canvertion of data type .
+# 1
+x="3"
+print(type(x))#type is use to check the data type
+print(int("3"))# str to int
+print(float("3"))#str to float
+#2
+y=3
+print(type(y))
+print(str(3))#int to str
+print(float(3))#int to flot
+#3
+z=3.56
+print(type(z))
+print(str(3.56))#float to str
+print(int(3.56))#float to int
+
+
+# how to print a malti line string
+# it is always like strirng which string coted double semicalomn and
+# but maltiline string cotted 3 semicalon
+#example
+a='''kjljljjkjlkjlkjkj
+khljlkjj'''
+print(f'malti line str>> {a}')
