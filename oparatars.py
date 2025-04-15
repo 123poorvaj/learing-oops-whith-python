af9c1b6d08946f6181a1a5a228db42ebe5ccd239
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-15 18:18:26 +0530

    Add files via upload

---
+++ oparatars.py
@@ -0,0 +1,35 @@
+#oparators
+#arthmetic oparators
+ # + Add
+ # - sub
+ # * multi
+ # ** power
+ # /  division
+ # //  div int
+
+# assingnment oparators
+# = equal
+# += add
+# -= sub
+
+#comparison oparators
+# == equals
+# <= less than or equal
+# >= grater than or equal
+# <   less than
+# >    grater than
+
+# logical oparators
+# and &
+# or |   bitwise oparators
+
+# identity oparators
+# is
+# is not
+
+# membership oparatar
+# in
+# not in
+a=1
+b=2
+print(a+b)
