0a9172e5e00c5f67592da03745225db9ce5701bd
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-06-13 14:13:01 +0530

    Add files via upload

--- hospetal management .py
+++ hospetal management .py
@@ -1,31 +1,19 @@
-from encodings.punycode import selective_find
-
-
-class doctor:
-    pass
-
-
 class patients:
     def __init__(self):
         self.name=input("Enter your name >>")
-        self.age=input("Enter your age >>")
-        self.__pament=0
-    def pditail(self,amount):
-        amount=int(input("pay the amount"))
-        self.__pament+=amount
+        self.age=int(input("Enter your age >>"))
+        self.__pement=0
+
+    def pement_(self):
+        amount = int(input("pay the amount"))
+        self.__pement+= amount
         print(f'you pay {amount} amount')
 
+    def get_pement(self):
+        return self.__pement
 
-
-class appointment(doctor,patients):
-        pass
-
-
-
-
-
-
-def doctor():
+class docter(appointment):
+    def docter():
         print("1,Dr pattile as a dentist surgeons")
         print("2,Dr sunil as a  fear  surgeons")
         print("3,Dr poorvaj  as a braine surgeons ")
@@ -45,37 +33,41 @@ def doctor():
             case 5:
                 print("Dr nagesh  avilabel in coming at 7:30 PM to 5 :30 AM")
             case _:
-                print("enter valied option")
-
+                print("enter valied option try again")
+                docter()
         try:
             key=int(input("if take a appointment enter 1 else enter  2>>>"))
         except ValueError:
             print("enter only number 1 or 2 ,alphabetical latters are not accepted")
 
         if key==1:
-            patients()
-
+            p=patients()
+            pe=int(input("you pay the amount enter 1 or 2"))
+            if pe==1:
+                p.pement_()
+            else:
+                print("ok  byeee......")
         elif key==2:
             print("ok bye.........")
         else:
             print("enter only number 1 or 2 ,latters are not accepted")
 
 
-class appointment():
-    pass  
-
-
-
-
 
 print("======== WELL COME TO MY HOSPITAL ========")
 print("your a patient enter option 1 else enter option 2")
-option=int(input("enter option >>"))
-match option:
-    case 1:
-       doctor()
-    case _:
-        exit()
+try:
+    option=int(input("enter option >>"))
+    match option:
+        case 1:
+            docter.docter()
+        case _:
+            exit()
+except ValueError:
+    print("enter only numbers,alphabetical latters are not accepted")
+
+
+
 
 
 
