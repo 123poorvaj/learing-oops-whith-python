4a84448f502dc57d300e60fa78613afb31e72464
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-06-29 20:40:07 +0530

    Add files via upload

---
+++ list in python.py
@@ -0,0 +1,10 @@
+#list is use to store the multiple items
+#square bracket are used to create list
+list=[6,7,9,23,6,4,7]
+print(list)
+list.insert(2,8)
+print(list)
+list.remove(23)
+print(list)
+list.append(23)
+print(list)
