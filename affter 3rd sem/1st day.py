0319957bb2c01f83e5d8a98db00df46d5dd1c401
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-01-25 18:33:22 +0530

    Add files via upload

---
+++ affter 3rd sem/1st day.py
@@ -0,0 +1,26 @@
+#basic matha matical operatares
+#1.addtion using "+" symbol
+
+a=56
+b=5
+print(a+b)
+
+#2.substraction using "-" symbol
+print(f"substraction",a-b)
+#3.maltipllication using '*' symbol
+print(f"maltiplication",a*b)
+#4.divison get float value using '/' symbol
+print(f"division get float value",a/b)
+#5.division get int value using '//' symbol
+print(f"divion of a and b  is {a//b}")
+#6.get remander using '%' symbol
+print(f"remander {a%b}")
+#HOW TO calculate power
+print(f"a power of b is {a**b}")
+
+c=(a+b)*(b/a)*(a+b) #this calculation folllows BODMAS ROOLE
+print(c)
+print("===============================")
+#how to write string type
+d="poorvaj"
+print(d)
