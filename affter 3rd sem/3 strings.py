5c8efb4b98c0c1bcf81611cdab59555aadd4b0cd
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-01-26 21:44:06 +0530

    Add files via upload

---
+++ affter 3rd sem/3 strings.py
@@ -0,0 +1,17 @@
+
+#multilin strings are write using threeable quates
+a='''poorvsj 
+ksdjskjdf sdfksjdfhskdf'''
+print(a)
+
+#string acces elements
+#     01234567891011
+name='poorvajgowda' #string value store in the form of index
+print(name[0]) #p
+print(name[2:]) #orvajgowda ,exaple [start:]
+print(name[3:5]) #[start : end-1] ,example [3: 5-1]=[3:4]
+print(name[7:10])
+#only evennumber or one-one diferent.
+#[start:end-1:skip] #[start:end-1:skip-1]
+print(name[3: :2])
+print(name[2: :1])
