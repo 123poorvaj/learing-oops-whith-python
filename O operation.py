d4115e20083a28be768ac689682e9fee15125129
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-03-08 00:35:02 +0530

    Add files via upload

---
+++ O operation.py
@@ -0,0 +1,13 @@
+
+a=input("Enter str 1:") #python read the input in string type
+b=input('Enter str 2: ')
+#how concatinate/join the strings?
+# by adding string we achive the concatination/join
+print('----------cancatinate--------')
+print(a+" "+b) 
+#repeating the string by multiplay with integers
+print('--------------string reapeatation-----------')
+print((a+' ')*3)
+#find the leanth of the string using len function
+print('----------------length---------------')
+print('lenght of the string: ',len(a))
