e4afa9ea0bede17205c272e2688f656e0f985e8d
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-03-10 00:11:12 +0530

    Add files via upload

--- relational operaters.py
+++ relational operaters.py
@@ -1,9 +1,30 @@
 #relational operatera are use to ckeck the relation of the two conditions
 #it returns the result in True or Flase 
 
-#greater than (>)
+# 1.greater than (>)
 print(3>2) #o/p True
+print(1>2) #o/p False
 
-#less than(<)
+
+# 2. less than(<)
 print(2<3)#o/p True
 print(2<1) #o/p flase 
+
+
+# 3. less than or equal to 
+print(2<=3) #o/p Ture
+print(3<=3)  #o/p True
+print(3<=2)  #o/p flase 
+
+# 4. greate than or equal to 
+print(3>=2)  #o/p True
+print(2>=2)  #o/p True
+print(2>=3)  #o/p Flase 
+
+#5. == equal to 
+print(2==2)  #o/p True
+print(2==1)  #o/p False
+
+#6. != not equal to
+print(2!=1)  #o/p True
+print(2!=2)  #o/p Folse
