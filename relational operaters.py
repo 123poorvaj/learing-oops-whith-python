1e46db4edf0357c52af74885971e4b5d14b0bb24
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-03-09 23:53:49 +0530

    Add files via upload

---
+++ relational operaters.py
@@ -0,0 +1,9 @@
+#relational operatera are use to ckeck the relation of the two conditions
+#it returns the result in True or Flase 
+
+#greater than (>)
+print(3>2) #o/p True
+
+#less than(<)
+print(2<3)#o/p True
+print(2<1) #o/p flase 
